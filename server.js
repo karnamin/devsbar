267ad1ccf01e38cd84b533b80525d02cba9f01c0
karnamin <aminkarn18@gmail.com>
2021-11-27 02:58:01 +0530

    added comments to server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express'); // importing express function
 
-const app = express();
+const app = express(); // app has express method
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running')); // creating '/' endpoint
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // process.env.port for heroku, localhost 5000 for development
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); // listening for connections on PORT
