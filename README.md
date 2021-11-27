dc3d967c3a7e82c753935679a12bd0a7db3db70a
karnamin <aminkarn18@gmail.com>
2021-11-27 18:51:41 +0530

    added model for users

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 A social media network application for developers.
 
 This project was built using the MERN (MongoDB, ExpressJS, ReactJS, and NodeJS) stack.
+
+
+## Scripts to run
+
+You can start the server with `npm run start`
---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose'); // importing mongoose
+
+// Creating Users schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.export = User = mongoose.model('user', UserSchema); // exporting Users model
