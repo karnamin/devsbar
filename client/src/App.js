440c19c009b3e349ae31dfe3e149b4499d36572d
karnamin <aminkarn18@gmail.com>
2022-01-19 16:54:56 +0530

    added react-router and login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => (
-    <Fragment className='App'>
-        <Navbar />
-        <Landing />
-    </Fragment>
+    <Router>
+        <Fragment>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' element={<Landing />} />
+                <Route exact path='/register' element={<Register />} />
+                <Route exact path='/login' element={<Login />} />
+            </Routes>
+        </Fragment>
+    </Router>
 );
 
 export default App;
