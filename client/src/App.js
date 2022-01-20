d6ff0fab4056c96d995b705dcc5376f2c76c45d0
karnamin <aminkarn18@gmail.com>
2022-01-20 17:18:22 +0530

    added redux and created redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,26 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-    <Router>
-        <Fragment>
-            <Navbar />
-            <Routes>
-                <Route exact path='/' element={<Landing />} />
-                <Route exact path='/register' element={<Register />} />
-                <Route exact path='/login' element={<Login />} />
-            </Routes>
-        </Fragment>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Routes>
+                    <Route exact path='/' element={<Landing />} />
+                    <Route exact path='/register' element={<Register />} />
+                    <Route exact path='/login' element={<Login />} />
+                </Routes>
+            </Fragment>
+        </Router>
+    </Provider>
 );
 
 export default App;
---
+++ client/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// initial state
+// empty as state will be in reducers
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
